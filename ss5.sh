005619fed3acdf16f99666e9106b2b8796ebfe28
ttagxwr <120248770+ttagxwr@users.noreply.github.com>
2022-12-16 02:20:39 +0800

    Update ss5.sh

--- ss5.sh
+++ ss5.sh
@@ -259,11 +259,11 @@ menu() {
     echo -e "\t无法使用请联系1853479098@qq.com\n"
 
     echo -e "—————————————— 安装向导 ——————————————"
-    echo -e "${Green} 搭建 站群多IP ss5 L2TP PPTP http ss v2ray 联系QQ1853479098${Font}"
+    echo -e "${Green}  ${Font}"
 
-    echo -e "${Red} QQ1853479098  ${Font}"
+    echo -e "${Red}   ${Font}"
 
-    echo -e "${Red} 出售美国香港多IP站群 服务器 ${Font}"
+    echo -e "${Red}  ${Font}"
     echo -e "${Green}1.${Font}  安装ss5"
     echo -e "${Green}2.${Font}  停止ss5"
     echo -e "${Green}3.${Font}  删除ss5"
